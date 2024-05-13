dbcdac46f0b7ae74d1b72a472883aec07c2906a2
rahul45 <rahulsb210@gmail.com>
2024-05-13 23:09:42 +0530

    all

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { FmsComponent } from './fms/fms.component';
+import { AutherizationComponent } from './autherization/autherization.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'details',component:AutherizationComponent},
+    {path:'',component:FmsComponent}
+];
